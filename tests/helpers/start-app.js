a760a84395500b4e4a20e54843b050f689330ca0
Christopher Garrett <me@pzuraq.com>
2017-09-26 14:01:57 -0700

    fix lint

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,13 +1,13 @@
-import Ember from 'ember';
+import { run } from '@ember/runloop';
 import Application from '../../app';
 import config from '../../config/environment';
 
 export default function startApp(attrs) {
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  let attributes = Object.assign({}, config.APP);
+  attributes = Object.assign(attributes, attrs); // use defaults, but you can override;
 
-  return Ember.run(() => {
-    let application = Application.create(attributes);
+  return run(() => {
+    const application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
     return application;
