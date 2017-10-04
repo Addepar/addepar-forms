6c43274a972a47049eebac62510754271270aa67
Lian Liu <lian.liu@addepar.com>
2017-10-04 15:32:55 -0700

    basic setup for form and input

--- addon/components/ice-input.js
+++ addon/components/ice-input.js
@@ -2,5 +2,6 @@ import TextField from '@ember/component/text-field';
 import layout from '../templates/components/ice-input';
 
 export default TextField.extend({
+  classNames: ['ice-input'],
   layout
 });
