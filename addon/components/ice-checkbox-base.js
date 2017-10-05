96c0466c12653cd86b924e6f24c39a5cda21cb29
Lian Liu <lian.liu@addepar.com>
2017-10-05 14:16:06 -0700

    first draft for checkbox styling

---
+++ addon/components/ice-checkbox-base.js
@@ -0,0 +1,22 @@
+// https://github.com/firefly-ui/ff-checkbox/blob/master/addon/components/ff-checkbox-base.js
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'input',
+  value: null,
+  type: 'checkbox',
+
+  attributeBindings: ['type', 'checked', 'disabled', 'indeterminate', 'value'],
+
+  change() {
+    const checked = this.element.checked;
+    const indeterminate = this.element.indeterminate;
+    const value = this.get('value');
+
+    this.element.checked = this.get('checked');
+    this.element.indeterminate = this.get('indeterminate');
+
+    this.sendAction('on-change', checked, { value, indeterminate });
+  },
+});
