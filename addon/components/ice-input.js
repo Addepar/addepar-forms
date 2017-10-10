fbdb572baf4aa36c5c15bbfa1b0712050f4de368
Lian Liu <lian.liu@addepar.com>
2017-10-10 15:04:27 -0700

    input updated

--- addon/components/ice-input.js
+++ addon/components/ice-input.js
@@ -5,5 +5,19 @@ export default Ember.Component.extend({
   classNames: ['ice-input'],
   layout,
   label: null,
-  helpText: null
+  helpText: null,
+
+  /**
+   * If true, this component will auto focus itself after insertion into the dom
+   * @type {boolean}
+  */
+  shouldFocus: false,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.get('shouldFocus')) {
+      this.$("input").focus();
+    }
+  },
 });
