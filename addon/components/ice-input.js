4f238ded1edb545df704a4c70a6fbad9c7f72008
Lian Liu <lian.liu@addepar.com>
2017-10-17 22:32:35 -0700

    input basic styling and demo

---
+++ addon/components/ice-input.js
@@ -0,0 +1,66 @@
+import Ember from 'ember';
+
+const { computed, guidFor, Component, TextField } = Ember;
+
+export default TextField.extend({
+  classNameBindings: ['inputClass', 'error:is-error', 'warning:is-warning', 'className'],
+  className: '',
+  inputSize: '',
+  error: false,
+  warning: false,
+  inputId: computed(function(){
+    return `input-${guidFor(this)}`;
+  }),
+  inputClass: computed('inputSize', function() {
+    let cls = 'ice-input';
+    const size = this.get('inputSize');
+
+    if (size === 'small') {
+      cls = `${cls}-sm`;
+    }
+
+    return cls;
+  }),
+
+  /**
+   * If true, this component will auto focus itself after insertion into the dom
+   * @type {boolean}
+  */
+  shouldFocus: false,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.get('shouldFocus')) {
+      this.$().focus();
+    }
+  },
+
+  change(e) {
+    this.sendAction('onChange', e);
+  },
+
+  focusIn(e) {
+    this.sendAction('onFocusIn', e);
+  },
+
+  //onBlur?
+  focusOut(e) {
+    this.sendAction('onFocusOut', e);
+  },
+
+  keyDown(e) {
+    this.sendAction('onKeyDown', e);
+  },
+
+  keyPress(e) {
+    // rn I assume we only cares about the case when enter is pressed
+    if(e.keyCode === 13) {
+      this.sendAction('onKeyPress', e);
+    }
+  },
+
+  keyUp(e) {
+    this.sendAction('onKeyUp', e);
+  },
+});
